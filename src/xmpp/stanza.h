dfe7e7c3f639f4076210c1f81e2f7c57f40b3b85
Michael Vetter <jubalh@iodoru.org>
2020-10-28 15:57:53 +0100

    wip: add rsm after we get the first couple of mam messages

--- src/xmpp/stanza.h
+++ src/xmpp/stanza.h
@@ -115,6 +115,7 @@
 #define STANZA_NAME_MINIMIZE         "minimize"
 #define STANZA_NAME_FIN              "fin"
 #define STANZA_NAME_LAST             "last"
+#define STANZA_NAME_AFTER            "after"
 
 // error conditions
 #define STANZA_NAME_BAD_REQUEST             "bad-request"
@@ -382,6 +383,6 @@ xmpp_stanza_t* stanza_get_child_by_name_and_ns(xmpp_stanza_t* const stanza, cons
 
 xmpp_stanza_t* stanza_create_avatar_retrieve_data_request(xmpp_ctx_t* ctx, const char* stanza_id, const char* const item_id, const char* const jid);
 
-xmpp_stanza_t* stanza_create_mam_iq(xmpp_ctx_t* ctx, const char* const jid, const char* const startdate);
+xmpp_stanza_t* stanza_create_mam_iq(xmpp_ctx_t* ctx, const char* const jid, const char* const startdate, const char *const lastid);
 
 #endif
